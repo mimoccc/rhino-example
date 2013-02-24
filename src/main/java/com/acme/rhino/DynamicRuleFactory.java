b68743e20c1b1230cd9d72f7d494247da42c4f46
Oliver Schrenk <oliver.schrenk@gmail.com>
2013-02-25 00:43:28 +0100

    Make Target constructor available to all classes
    See [1], [2]
    [1] https://github.com/tschaub/stackoverflow-q14561874/compare/alt1
    [2]
    http://stackoverflow.com/questions/14561874/calling-jsfunction-from-javascript-typeerror-cannot-find-default-value-for-ob/14588793#14588793

--- src/main/java/com/acme/rhino/DynamicRuleFactory.java
+++ src/main/java/com/acme/rhino/DynamicRuleFactory.java
@@ -2,6 +2,7 @@ package com.acme.rhino;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ScriptableObject;
@@ -16,9 +17,16 @@ public class DynamicRuleFactory {
 		final Context context = Context.enter();
 		try {
 			final ScriptableObject scope = context.initStandardObjects();
+			ScriptableObject.defineClass(scope, Target.class, false, true);
 			context.evaluateString(scope, script, "script", 1, null);
 			return new DynamicRule(context, scope);
 
+		} catch (final IllegalAccessException e) {
+			throw new IllegalArgumentException("Can't evaluate Script.", e);
+		} catch (final InstantiationException e) {
+			throw new IllegalArgumentException("Can't evaluate Script.", e);
+		} catch (final InvocationTargetException e) {
+			throw new IllegalArgumentException("Can't evaluate Script.", e);
 		} finally {
 			Context.exit();
 		}
